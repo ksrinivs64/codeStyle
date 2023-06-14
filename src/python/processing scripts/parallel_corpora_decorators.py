7d07e71cb2c148a8ff34c54a150e1d96739396ba
unknown <ksmunson@ucsc.edu>
2023-06-14 07:01:50 -0700

    cleaned processing scripts and updated readme

--- src/python/processing scripts/parallel_corpora_decorators.py
+++ src/python/processing scripts/parallel_corpora_decorators.py
@@ -1,12 +1,21 @@
 import ast
 import pandas as pd
 from tqdm import tqdm
-
+import os
+import sys
 import astunparse
 class toFuncLower(ast.NodeTransformer):
     def visit_FunctionDef(self, node):
         node.decorator_list = []
         return node
+    def visit_AsyncFunctionDef(self, node):
+        node.decorator_list = []
+        #print("async decorators stripped")
+        return node
+    def visit_ClassDef(self, node):
+        node.decorator_list = []
+        #print("async classes")
+        return node    
         # return ast.FunctionDef(**{**node.__dict__, 'name':node.name.lower().replace("_","")})
 
 
@@ -31,10 +40,10 @@ def remove_decorator(input_code):
 
 if __name__ == "__main__":
     processed_scripts = []
-    data_input = pd.read_csv("bq_uncommented_outlier.csv")
+    data_input = pd.read_csv(sys.argv[1])
     print("Length of input data : ", len(data_input))
     for data in tqdm(data_input["content"]):
         processed_script = remove_decorator(data)
         processed_scripts.append(processed_script)
     data_input["decorator_modified"] = processed_scripts
-    data_input.to_csv("bq_individual_no_decorators.csv")
+    data_input.to_csv("transformed_no_decorators.csv")
