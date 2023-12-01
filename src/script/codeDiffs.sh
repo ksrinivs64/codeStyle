fc9f1f333d7a9abdf1bcf4b788a240955e36b4e8
Julian Dolby <dolby@us.ibm.com>
2023-12-01 15:51:24 -0500

    sort

--- src/script/codeDiffs.sh
+++ src/script/codeDiffs.sh
@@ -6,10 +6,9 @@ DIR=`dirname $ME`
 INPUT=$1
 OUTPUT=$2
 EXPECTED=$3
+diff $INPUT $EXPECTED | gawk -f $DIR/codeDiffs.awk | sort > /tmp/expected_change.txt
 
-diff $INPUT $EXPECTED | gawk -f $DIR/codeDiffs.awk > /tmp/expected_change.txt
-
-diff $INPUT $OUTPUT | gawk -f $DIR/codeDiffs.awk  > /tmp/actual_change.txt
+diff $INPUT $OUTPUT | gawk -f $DIR/codeDiffs.awk  | sort > /tmp/actual_change.txt
 
 missing_changes=`comm -13  /tmp/expected_change.txt  /tmp/actual_change.txt | wc -l`
 
