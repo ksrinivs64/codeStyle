d6c1bb0b678f08fa0dc1c45592bf0cb7848e0867
Julian Dolby <dolby@us.ibm.com>
2023-12-07 21:08:32 +0000

    run python tests

---
+++ src/script/run_python_codenet.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for DAT in $1/*/Python; do
+    IO=`dirname $DAT`
+    IO=`basename $IO`
+    for s in `ls $DAT | egrep 's[0-9]*[.]py'`; do
+	STEM=`basename $s .py`
+	for p in $DAT/$STEM_*_input.py; do
+	    PROBLEM=`echo $p | sed 's/.*_\([^_]*\)_input.py/\1/'`
+	    for m in $DAT/${STEM}_${PROBLEM}_pred_*.py; do
+		MODEL=`echo $m | sed 's/.*_pred_\([^_]*\).py/\1/'`
+		echo $STEM $PROBLEM $MODEL $IO
+	    done
+	done
+    done
+done
