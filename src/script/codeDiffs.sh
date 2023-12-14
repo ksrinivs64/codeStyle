7ce011bf9f238bf7047d5dedbf3e4a5367de006e
Kavitha Srinivas <Kavitha.Srinivas@ibm.com>
2023-12-14 18:59:27 +0000

    add fixes

--- src/script/codeDiffs.sh
+++ src/script/codeDiffs.sh
@@ -10,8 +10,9 @@ trap 'rm -rf $WS' EXIT
 mkdir -p $WS
 
 if test $4="True"; then
-    python $DIR/../normalize.py $1 >$WS/input.py
+    python $DIR/../normalize.py $1 > $WS/input.py
     python $DIR/../normalize.py $3 > $WS/expected.py
+    python $DIR/../normalize.py $2 > $WS/output.py
 else
     cp $1 $WS/input.py
     cp $3 $WS/expected.py
@@ -19,7 +20,7 @@ fi
 
 diff -B $WS/input.py $WS/expected.py | gawk -f $DIR/codeDiffs.awk | sort > $WS/expected_change.txt
 
-diff -B $WS/input.py $2 | gawk -f $DIR/codeDiffs.awk  | sort > $WS/actual_change.txt
+diff -B $WS/input.py $WS/output.py | gawk -f $DIR/codeDiffs.awk  | sort > $WS/actual_change.txt
 
 missing_changes=`comm -23  $WS/expected_change.txt  $WS/actual_change.txt | wc -l`
 
@@ -29,4 +30,6 @@ expected_changes=`comm -12  $WS/expected_change.txt  $WS/actual_change.txt | wc 
 
 gold_expected=`cat $WS/expected_change.txt | wc -l`
 
-echo $expected_changes $missing_changes $unexpected_changes $gold_expected
+actual_change=`cat $WS/actual_change.txt | wc -l`
+
+echo $1 $expected_changes $missing_changes $unexpected_changes $gold_expected $actual_change
