2133260dfe7738a4c7059348a567706548acc766
Julian Dolby <dolby@us.ibm.com>
2024-01-04 16:15:21 -0500

    check for u pected stuff even when there is nothing to do

--- src/script/codeCompare.sh
+++ src/script/codeCompare.sh
@@ -21,40 +21,40 @@ else
     cp $3 $WS/expected
 fi
 
-if diff $1 $3 > /dev/null; then
-    echo "-1,-1,-1,-1"
-else
-    python $DIR/../python/ndiff.py ${WS}/input ${WS}/expected | egrep '^ -' | sort > ${WS}/expected_removals.txt
+python $DIR/../python/ndiff.py ${WS}/input ${WS}/expected | egrep '^ -' | sort > ${WS}/expected_removals.txt
     
-    python $DIR/../python/ndiff.py ${WS}/input ${WS}/output | egrep '^ -' | sort > ${WS}/actual_removals.txt
+python $DIR/../python/ndiff.py ${WS}/input ${WS}/output | egrep '^ -' | sort > ${WS}/actual_removals.txt
     
-    comm -23 ${WS}/expected_removals.txt ${WS}/actual_removals.txt > ${WS}/removal_diff
+comm -23 ${WS}/expected_removals.txt ${WS}/actual_removals.txt > ${WS}/removal_diff
     
-    if [[ -e ${WS}/removal_diff && ! -s ${WS}/removal_diff ]]; then
+if [[ -e ${WS}/removal_diff && ! -s ${WS}/removal_diff ]]; then
     removed_correctly=1
-    fi
+fi
     
-    comm -13 ${WS}/expected_removals.txt ${WS}/actual_removals.txt > ${WS}/removal_diff_2
+comm -13 ${WS}/expected_removals.txt ${WS}/actual_removals.txt > ${WS}/removal_diff_2
     
-    if [[ -e ${WS}/removal_diff_2 && ! -s ${WS}/removal_diff_2 ]]; then
+if [[ -e ${WS}/removal_diff_2 && ! -s ${WS}/removal_diff_2 ]]; then
     no_unexpected_removed=1
-    fi
+fi
     
-    python $DIR/../python/ndiff.py ${WS}/input ${WS}/expected | egrep '^ \+' | sort > ${WS}/expected_additions.txt
+python $DIR/../python/ndiff.py ${WS}/input ${WS}/expected | egrep '^ \+' | sort > ${WS}/expected_additions.txt
     
-    python $DIR/../python/ndiff.py ${WS}/input ${WS}/output | egrep '^ \+' | sort > ${WS}/actual_additions.txt
-    
-    comm -23 ${WS}/expected_additions.txt ${WS}/actual_additions.txt > ${WS}/addition_diff
+python $DIR/../python/ndiff.py ${WS}/input ${WS}/output | egrep '^ \+' | sort > ${WS}/actual_additions.txt
 
-    if [[ -e ${WS}/addition_diff && ! -s ${WS}/addition_diff ]]; then
-	added_correctly=1
-    fi
-    
-    comm -13 ${WS}/expected_additions.txt ${WS}/actual_additions.txt > ${WS}/addition_diff
-    
-    if [[ -e ${WS}/addition_diff && ! -s ${WS}/addition_diff ]]; then
-	no_unexpected_added=1
-    fi
+comm -23 ${WS}/expected_additions.txt ${WS}/actual_additions.txt > ${WS}/addition_diff
 
+if [[ -e ${WS}/addition_diff && ! -s ${WS}/addition_diff ]]; then
+    added_correctly=1
+fi
+    
+comm -13 ${WS}/expected_additions.txt ${WS}/actual_additions.txt > ${WS}/addition_diff
+    
+if [[ -e ${WS}/addition_diff && ! -s ${WS}/addition_diff ]]; then
+    no_unexpected_added=1
+fi
+
+if diff $1 $3 > /dev/null; then
+    echo "-1,-1,$no_unexpected_removed,$no_unexpected_added"
+else
     echo "$added_correctly,$removed_correctly,$no_unexpected_removed,$no_unexpected_added"
 fi
